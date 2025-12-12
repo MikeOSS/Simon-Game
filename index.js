a712b219b2d2fc4675bf03485085175604bac088
Miguel Costacurta <miguel.costacurta3@gmail.com>
2025-12-12 17:18:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const yellow = document.querySelector("#yellow");
 const history = [];
 //functions
 function playSound(color) {
-    var audio = new Audio(`sounds/${color}.mp3`);
-    audio.play();
+    // var audio = new Audio(`sounds/${color}.mp3`);
+    // audio.play();
+    console.log(color);
 }
 
 function callButton() {
@@ -15,42 +16,62 @@ function callButton() {
 
     history.push(randomNumber);
 
-    if (randomNumber === 1) {
-        playSound('blue');
-        blue.style.transform = 'scale(1.2)';
-        setTimeout(function () {
-            blue.style.transform = 'scale(1.0)';
-        }, 250);
+    for(i=0; i< history.length; i++){
+        if (randomNumber === 1) {
+            playSound('blue');
+            blue.style.transform = 'scale(1.2)';
+            setTimeout(function () {
+                blue.style.transform = 'scale(1.0)';
+            }, 250);
 
-        // blue.addEventListener('click', () => {
-        //     //     if (history[history.length - 1] === )
-        //     //         callButton();
-        //     // })
-    } else if (randomNumber === 2) {
-        playSound('green');
-        green.style.transform = 'scale(1.2)';
-        setTimeout(function () {
-            green.style.transform = 'scale(1.0)';
-        }, 250);
+            blue.addEventListener('click', () => {
+                callButton();
+            });
+            green.addEventListener('click', () => {
+                playSound('wrong');
+                return;
 
-    } else if (randomNumber === 3) {
-        playSound('red');
-        red.style.transform = 'scale(1.2)';
-        setTimeout(function () {
-            red.style.transform = 'scale(1.0)';
-        }, 250);
+            });
+            red.addEventListener('click', () => {
+                playSound('wrong');
+                return;
+            });
+            yellow.addEventListener('click', () => {
+                playSound('wrong');
+                return;
+            });
 
-    } else if (randomNumber === 4) {
-        playSound('yellow');
-        yellow.style.transform = 'scale(1.2)';
-        setTimeout(function () {
-            yellow.style.transform = 'scale(1.0)';
-        }, 250);
+        } else if (randomNumber === 2) {
+            playSound('green');
+            green.style.transform = 'scale(1.2)';
+            setTimeout(function () {
+                green.style.transform = 'scale(1.0)';
+            }, 250);
+            green.addEventListener('click', () => {
+                callButton();
+            })
 
+        } else if (randomNumber === 3) {
+            playSound('red');
+            red.style.transform = 'scale(1.2)';
+            setTimeout(function () {
+                red.style.transform = 'scale(1.0)';
+            }, 250);
+            red.addEventListener('click', () => {
+                callButton();
+            })
+
+        } else if (randomNumber === 4) {
+            playSound('yellow');
+            yellow.style.transform = 'scale(1.2)';
+            setTimeout(function () {
+                yellow.style.transform = 'scale(1.0)';
+            }, 250);
+            yellow.addEventListener('click', () => {
+                callButton();
+            })
+        }
     }
-
-
-
 }
 
 callButton();
